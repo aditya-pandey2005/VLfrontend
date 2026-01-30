e8cd81193aa55415badb3297df01d8bc8bb90595
Aditya <adityapandey@gmail.com>
2026-01-30 20:25:01 +0530

    done

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_URL = process.env.API_URL || "http://127.0.0.1:5000";
+export const API_URL = import.meta.env.process.env.API_URL || "https://thermosense.onrender.com";
 
 // Form Labels
 export const formLabels = {
